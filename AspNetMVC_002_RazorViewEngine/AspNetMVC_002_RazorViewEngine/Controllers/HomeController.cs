8967a19dcd1c1563bbc8b9a8afed886ce27f7077
HsynTrkmngl <turkmenoglu_bjk91@hotmail.com>
2017-01-29 19:53:51 +0300

    Added  AspNetMVC_Ornegi

---
+++ AspNetMVC_002_RazorViewEngine/AspNetMVC_002_RazorViewEngine/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetMVC_002_RazorViewEngine.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult BoslukYazdirma()
+        {
+            return View();
+        }
+
+        public ActionResult HtmlYazdirma()
+        {
+            return View();
+        }
+
+        public ActionResult Donguler()
+        {
+            return View();
+        }
+
+        public ActionResult DongulerOzelKodlama()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ AspNetMVC_002_RazorViewEngine/AspNetMVC_002_RazorViewEngine/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspNetMVC_002_RazorViewEngine.Startup))]
+namespace AspNetMVC_002_RazorViewEngine
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
